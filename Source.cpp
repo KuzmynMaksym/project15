449c3e0154804392076b317f8f516bc9bc8bddf2
KuzmynMaksym <144415346+KuzmynMaksym@users.noreply.github.com>
2023-10-26 11:20:24 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int a, b, c;
+
+    cin >> a;
+
+    b = a / 100;
+
+    c = a % 10;
+
+    if (b != c)
+
+        cout << "Всі цифри даного числа різні";
+
+    else
+
+        cout << "Не всі цифри даного числа різні";
+
+    return 0;
+
+}
+
